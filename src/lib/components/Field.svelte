32f02e23cfa581bc56f66c6eb5443a802f78c65e
gwen-pigat <pixofheaven@gmail.com>
2024-06-26 00:21:14 +0200

    init

--- src/lib/components/Field.svelte
+++ src/lib/components/Field.svelte
@@ -1,20 +1,14 @@
 <script>
-    import { onMount } from "svelte";
 
     export let type
     export let name
     export let value
 
-    onMount(() => {
-        let item = document.querySelector("#"+name)
-        item.type = item.getAttribute("data-type")
-    })
-
 </script>
 
 
 <input id={name} 
-    data-type={type} 
+    {...{ type }}
     {name} 
     placeholder={name} 
     bind:value={value} 
