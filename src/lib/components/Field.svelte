b9a0d400627afdfb7398509f62f76c6ccfaf1aab
gwen-pigat <pixofheaven@gmail.com>
2024-06-26 00:11:37 +0200

    init

---
+++ src/lib/components/Field.svelte
@@ -0,0 +1,21 @@
+<script>
+    import { onMount } from "svelte";
+
+    export let type
+    export let name
+    export let value
+
+    onMount(() => {
+        let item = document.querySelector("#"+name)
+        item.type = item.getAttribute("data-type")
+    })
+
+</script>
+
+
+<input id={name} 
+    data-type={type} 
+    {name} 
+    placeholder={name} 
+    bind:value={value} 
+    required />
---
+++ src/lib/components/Form.svelte
@@ -0,0 +1,38 @@
+<script>
+    import Field from "./Field.Svelte";
+    let attendee = {
+        "username": "Test",
+        "civility":"M",
+        "login":"gwenael@captusite.fr"
+    }
+    let otherAttendee = {
+        "username":"Doe",
+        "civility":"Mme",
+        "login":"test@captusite.fr"
+    }
+    const switchAttendee = () => {
+        attendee = {...otherAttendee}
+    }
+
+</script>
+
+
+<table>
+    <tr>
+        <td>{otherAttendee['username']}</td>
+        <td>{otherAttendee['civility']}</td>
+        <td>
+            <button type="button" 
+                on:click={switchAttendee}>
+                Edit
+            </button>
+        </td>
+    </tr>
+</table>
+
+<form id="setData">
+    <Field type="text" name="civility"  value={attendee['civility']} />
+    <Field type="text" name="username"  value={attendee['username']} />
+    <Field type="email" name="login"    value={attendee['login']} />
+    <button type="submit">Envoyer</button>
+</form>
